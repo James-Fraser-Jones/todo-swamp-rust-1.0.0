4918986d8a64824d189e1030ef3e3d32abe2fad5
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-04-08 17:19:09 +0100

    established a good basic setup for benchmarking

--- src/bin/application.rs
+++ src/bin/application.rs
@@ -7,13 +7,23 @@ use std::fs;
 use std::time;
 
 pub fn main() -> io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    let file_name = &args[1];
+    // let args: Vec<String> = std::env::args().collect();
+    // let file_name = &args[1];
 
-    test_run(file_name, "_output", TriedoList::<Trie4>::new(), Some(10000), false)
+    //compare implementations
+    for i in 1..=4 {
+        let naive = benchmark_run(&format!("tests/test{}", i), TodoList::new(), 5000)?;
+        let trie1 = benchmark_run(&format!("tests/test{}", i), TriedoList::<Trie1>::new(), 5000)?;
+        let trie2 = benchmark_run(&format!("tests/test{}", i), TriedoList::<Trie2>::new(), 5000)?;
+        let trie3 = benchmark_run(&format!("tests/test{}", i), TriedoList::<Trie3>::new(), 5000)?;
+        let trie4 = benchmark_run(&format!("tests/test{}", i), TriedoList::<Trie4>::new(), 5000)?;
+        println!("Naive: {}, Trie1: {}, Trie2: {}, Trie3: {}, Trie4: {}", naive, trie1, trie2, trie3, trie4);
+    }
+
+    Ok(())
 }
 
-pub fn standard_run() -> io::Result<()> {
+fn _standard_run() -> io::Result<()> {
     let stdin = io::stdin();
     let stdout = io::stdout();
 
@@ -34,31 +44,49 @@ pub fn standard_run() -> io::Result<()> {
     Ok(())
 }
 
+//returns number of responses (search results count as a single response)
+fn benchmark_run<T: TodoLister>(file_name: &str, mut tl: T, max_millis: u128) -> io::Result<usize> {
+    let file_in = fs::File::open(format!("{}.in", file_name))?;
+    let mut lines_in = io::BufReader::new(file_in).lines();
+    let mut count = 0;
+    if let Some(Ok(_s)) = lines_in.next() {
+        let start = time::Instant::now();
+        for line in lines_in {
+            if start.elapsed().as_millis() > max_millis {
+                break
+            }
+            if let Ok(l) = line {
+                if let Some(_) = runner::run_line(&l, &mut tl) {
+                    count += 1;
+                }
+            }
+        }
+    }
+    Ok(count)
+}
+
 /* Command to test resulting files when sort == true (0 means files are equal)
 cmp tests/Ak/benchmark_Ak_N_test.out tests/Ak/benchmark_Ak_N_testB.out ; echo $?
 */
-pub fn test_run<T: TodoLister>(file_name: &str, append: &str, mut tl: T, millis: Option<u128>, sort: bool) -> io::Result<()> {
+fn _test_run<T: TodoLister>(file_name: &str, append: &str, mut tl: T, num_lines: usize) -> io::Result<()> {
     let file_in = fs::File::open(format!("{}.in", file_name))?;
     let file_out = fs::File::create(format!("{}{}.out", file_name, append))?;
     let mut lines_in = io::BufReader::new(file_in).lines();
     let mut buffer_out = io::BufWriter::new(file_out);
+    let mut count = 0;
     if let Some(Ok(_s)) = lines_in.next() {
-        let start = time::Instant::now();
         for line in lines_in {
-            if let Some(millis) = millis {
-                if start.elapsed().as_millis() > millis {
-                    writeln!(buffer_out, "TASK FORCED TO EXIT AFTER: {}MS", millis)?;
-                    break
-                }
+            if count >= num_lines {
+                break
             }
             if let Ok(l) = line {
                 if let Some(mut r) = runner::run_line(&l, &mut tl) {
-                    if sort {
-                        if let QueryResult::Found(results) = &mut r { 
-                            results.sort(); //sorted results makes resulting test files easy to check for equality
-                        }
+                    if let QueryResult::Found(results) = &mut r { 
+                        results.sort();             //sorted results makes resulting test files easy to check for equality
+                        count += results.len() - 1; //search results can produce more than one line, count needs to be updated accordingly
                     }
                     writeln!(buffer_out, "{}", r)?;
+                    count += 1;
                 }
                 else { 
                     panic!(); //make bugs more apparent
