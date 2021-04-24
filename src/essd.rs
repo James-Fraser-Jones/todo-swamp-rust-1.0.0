a247733f1d883d88f654365a5b6c4579e3425cb0
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-04-24 14:44:20 +0100

    added node initialization, need to find reasonable linked list implementation for storing linked-ids

--- src/essd.rs
+++ src/essd.rs
@@ -5,17 +5,17 @@ use std::collections::{HashMap, HashSet};
 use std::ptr;
 use arrayvec::ArrayVec;
 
-const K: usize = 3;         //TERMINOLOGY: number of symbols in alphabet (TODO: should really use a macro to generate from Sigma definition)
-const M: usize = 10;        //TERMINOLOGY: max length of attributes
-const NoLink: Link = None;  //Necessary for initializing arrays
+const CARDINALITY: usize = 3;           //TERMINOLOGY K: number of symbols in alphabet (TODO: should really use a macro to generate from Sigma definition)
+const ATTR_MAX: usize = 10;             //TERMINOLOGY M: max length of attributes
+const EMPTY_NODE_LINK: NodeLink = None; //Necessary for initializing arrays
 
 type Level = usize;
 type Position = usize;
 type Id = usize;
-type Link = Option<Box<Node>>; //inspired by: https://rust-unofficial.github.io/too-many-lists/second-final.html
+type NodeLink = Option<Box<Node>>; //inspired by: https://rust-unofficial.github.io/too-many-lists/second-final.html
 
 #[derive(Clone)]
-enum Sigma { //TERMINOLOGY: alphabet of symbols
+enum Sigma { //TERMINOLOGY Σ: alphabet of symbols
     A, B, C,
 }
 impl From<char> for Sigma {
@@ -37,7 +37,7 @@ impl From<Sigma> for char {
         }
     }
 }
-impl From<Sigma> for usize { //for navigating [T; K] arrays, (TODO: we actually want a macro for making this substitution at compile-time)
+impl From<Sigma> for usize { //for navigating [T; CARDINALITY] arrays, (TODO: we actually want a macro for making this substitution at compile-time)
     fn from(s: Sigma) -> Self {
         match s {
             Sigma::A => 0,
@@ -46,18 +46,18 @@ impl From<Sigma> for usize { //for navigating [T; K] arrays, (TODO: we actually 
         }
     }
 }
-impl Default for Sigma {
+impl Default for Sigma { //necessary for root node
     fn default() -> Self {
         Sigma::A
     }
 }
 
 #[derive(Clone)]
-struct SigString(ArrayVec<Sigma, M>);
+struct SigString(ArrayVec<Sigma, ATTR_MAX>);
 impl From<&str> for SigString {
     fn from(s: &str) -> Self {
         let mut vec = ArrayVec::new();
-        for c in s.chars().take(M) {
+        for c in s.chars().take(ATTR_MAX) {
             vec.push(Sigma::from(c));
         }
         SigString(vec)
@@ -75,29 +75,38 @@ impl From<SigString> for String {
 
 struct Essd { 
     table: Vec<SigString>, //id = vector index (we never need to delete items from the table, only make them unsearchable through the trie)
-    trie: Link,
+    trie: NodeLink,
     //linked_ids: ...
 }
 impl Essd {
-    fn n(&self) -> usize { //TERMINOLOGY: number of tuples in table
+    fn table_length(&self) -> usize { //TERMINOLOGY N: number of tuples in table
         self.table.len()
     }
-    fn node(&self, x: Position, y: Level) -> Option<&Node> { //TERMINOLOGY: refers to node at position x, level y
+    fn node(&self, x: Position, y: Level) -> Option<&Node> { //TERMINOLOGY node(x, y): refers to node at position x, level y
         panic!() //awkward to implement and probably unnecessary? (TODO: Maybe implement?)
     }
 }
 impl Essd {
     fn new() -> Self {
+        let root_node = Node::new(
+            ptr::null_mut(),
+            ptr::null_mut(),
+            Sigma::default(), 
+            0,
+            0,
+            ptr::null_mut(),
+            ATTR_MAX,
+        );
         Essd {
             table: Vec::new(),
-            trie: Some(Box::new(Node::new(0, M))), //root node
+            trie: Some(Box::new(root_node)), //root node
         }
     }
     fn insert(&mut self, attribute: SigString) { //No id necessary here since Essd.table: Vec<SigString>
         unimplemented!()
     }
     fn search(&self, SigString(query): SigString) -> Vec<(Id, SigString)> {
-        let _l = query.len(); //TERMINOLOGY: length of given query (l <= m)
+        let _query_length = query.len(); //TERMINOLOGY L: length of given query (l <= m)
         let trie = self.trie.as_ref().unwrap();
         let ids = trie.search(&query);
         let mut results = Vec::new();
@@ -112,30 +121,38 @@ impl Essd {
 }
 
 struct Node {
-    children: [Link; K],
+    children: [NodeLink; CARDINALITY],
     start_tuple: *mut Id,
     end_tuple: *mut Id,
-    label: Sigma,                       //Sigma::default() for root node                (should never be used at root node)
-    first_occour: Vec<[*mut Node; K]>,  //e.g. first_occour[5][usize::from(Sigma::A)]   (find first fresh occourence of A at relative level 5)
-    last_occour: Vec<[*mut Node; K]>,
-    level: Level,                       //0 for root node
+    label: Sigma,                                   //Sigma::default() for root node                (should never be used at root node)
+    first_occour: Vec<[*mut Node; CARDINALITY]>,    //e.g. first_occour[5][usize::from(Sigma::A)]   (find first fresh occourence of A, 6 levels beneath this node)
+    last_occour: Vec<[*mut Node; CARDINALITY]>,
+    level: Level,                                   //0 for root node
     next: *mut Node,
-    position: Position,                 //0 for root and first-inserted node at each level
+    position: Position,                             //0 for root and first-inserted node at each level
     parent: *mut Node,
 }
 impl Node {
-    fn new(level: usize, max_level: usize) -> Self {
+    fn new(
+        start_tuple: *mut Id, 
+        end_tuple: *mut Id,
+        label: Sigma,
+        level: usize,
+        position: usize,
+        parent: *mut Node,
+        max_level: usize,
+        ) -> Self {
         Node {
-            children: [NoLink; K],
-            start_tuple: ptr::null_mut(),
-            end_tuple: ptr::null_mut(),
-            label: Sigma::default(),
-            first_occour: vec![[ptr::null_mut(); K]; max_level - level],
-            last_occour: vec![[ptr::null_mut(); K]; max_level - level],
-            level,
+            children: [EMPTY_NODE_LINK; CARDINALITY],
+            first_occour: vec![[ptr::null_mut(); CARDINALITY]; max_level - level],
+            last_occour: vec![[ptr::null_mut(); CARDINALITY]; max_level - level],
             next: ptr::null_mut(),
-            position: 0, //TODO: figure out how this gets calculated and utilized
-            parent: ptr::null_mut(),
+            start_tuple,
+            end_tuple,
+            label,
+            level,
+            position,
+            parent,
         }
     }
     fn insert(&mut self, id: Id, attribute: &[Sigma]) { //does not support update (i.e. id should not already exist)
