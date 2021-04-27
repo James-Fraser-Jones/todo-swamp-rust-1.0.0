f0871984f136e08b5e7eb28d3d495860bc69303e
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-04-27 16:48:03 +0100

    changed data representation to store trie as array of vectors

--- src/essd.rs
+++ src/essd.rs
@@ -1,22 +1,24 @@
 //Implementation of Trie algorithm from "Efficient Subsequence Search for Databases"
 //https://link.springer.com/chapter/10.1007/978-3-642-38562-9_45
 
-use std::collections::{HashMap, HashSet};
-use std::ptr;
+use std::collections::HashSet;
 use arrayvec::ArrayVec;
 
-const CARDINALITY: usize = 3;           //TERMINOLOGY K: number of symbols in alphabet (TODO: should really use a macro to generate from Sigma definition)
-const ATTR_MAX: usize = 10;             //TERMINOLOGY M: max length of attributes
-const EMPTY_NODE_LINK: NodeLink = None; //Necessary for initializing arrays
+const CARDINALITY: usize = 3;   //TERMINOLOGY K: number of symbols in alphabet (does not include special 'Root' symbol)
+const ATTR_MAX: usize = 10;     //TERMINOLOGY M: max length of attributes
 
-type Level = usize;
-type Position = usize;
-type Id = usize;
-type NodeLink = Option<Box<Node>>; //inspired by: https://rust-unofficial.github.io/too-many-lists/second-final.html
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Id(usize);
+struct Lvl(usize);
+struct Pos(usize);
+type PosPtr = Option<Pos>;
+
+const POS_NULL: PosPtr = None;  //Necessary for initializing arrays
+const EMPTY_NODES: Vec<Node> = Vec::new();
 
 #[derive(Clone)]
 enum Sigma { //TERMINOLOGY Σ: alphabet of symbols
-    A, B, C,
+    A, B, C, Root,
 }
 impl From<char> for Sigma {
     fn from(c: char) -> Self {
@@ -34,21 +36,18 @@ impl From<Sigma> for char {
             Sigma::A => 'a',
             Sigma::B => 'b',
             Sigma::C => 'c',
+            _ => panic!(),
         }
     }
 }
-impl From<Sigma> for usize { //for navigating [T; CARDINALITY] arrays, (TODO: we actually want a macro for making this substitution at compile-time)
+impl From<Sigma> for usize { //for indexing arrays, (TODO: we actually want a macro for making this substitution at compile-time)
     fn from(s: Sigma) -> Self {
         match s {
             Sigma::A => 0,
             Sigma::B => 1,
             Sigma::C => 2,
+            _ => panic!(),
         }
-    }
-}
-impl Default for Sigma { //necessary for root node
-    fn default() -> Self {
-        Sigma::A
     }
 }
 
@@ -75,31 +74,33 @@ impl From<SigString> for String {
 
 struct Essd { 
     table: Vec<SigString>, //id = vector index (we never need to delete items from the table, only make them unsearchable through the trie)
-    trie: NodeLink,
-    linked_ids: Vec<(Id, usize, usize)>,   //hacky way of getting linked-list-like functionality (TODO: Possibly remove prev_index if we don't need it)
+    trie: [Vec<Node>; ATTR_MAX+1],
+    linked_ids: Vec<(Id, Option<usize>)>, //hacky way of getting linked-list-like functionality
 }
 impl Essd {
     fn table_length(&self) -> usize { //TERMINOLOGY N: number of tuples in table
         self.table.len()
     }
-    fn node(&self, x: Position, y: Level) -> Option<&Node> { //TERMINOLOGY node(x, y): refers to node at position x, level y
-        panic!() //(TODO: implement)
+    fn node(&self, Pos(x): Pos, Lvl(y): Lvl) -> Option<&Node> { //TERMINOLOGY node(x, y): refers to node at position x, level y
+        self.trie.get(y).and_then(|vec| vec.get(x))
+    }
+    fn root(&self) -> &Node {
+        self.node(Pos(0), Lvl(0)).unwrap()
     }
 }
 impl Essd {
     fn new() -> Self {
         let root_node = Node::new(
-            std::usize::MAX,
-            std::usize::MAX,
-            Sigma::default(), 
-            0,
-            0,
-            ptr::null_mut(),
-            ATTR_MAX,
+            Sigma::Root, 
+            Lvl(0),
+            Pos(0),
+            POS_NULL,
         );
+        let mut trie = [EMPTY_NODES;ATTR_MAX+1];
+        trie[0].push(root_node);
         Essd {
             table: Vec::new(),
-            trie: Some(Box::new(root_node)), //root node
+            trie, //root node
             linked_ids: Vec::new(),
         }
     }
@@ -108,11 +109,11 @@ impl Essd {
     }
     fn search(&self, SigString(query): SigString) -> Vec<(Id, SigString)> {
         let _query_length = query.len(); //TERMINOLOGY L: length of given query (L <= M)
-        let trie = self.trie.as_ref().unwrap();
-        let ids = trie.search(&query, &self.linked_ids);
+        let root_node = self.root();
+        let ids = root_node.search(&query, &self.trie, &self.linked_ids);
         let mut results = Vec::new();
         for id in ids {
-            results.push((id, self.table[id].to_owned()));
+            results.push((id, self.table[id.0].to_owned()));
         }
         results
     }
@@ -122,36 +123,27 @@ impl Essd {
 }
 
 struct Node {
-    children: [NodeLink; CARDINALITY], //TODO: Implement with pointers instead and have Vec<Vec<Node>> parametrized by level then position
-
-    start_id_index: usize,                          //std::usize::MAX in the case of node containing no ids
-    end_id_index: usize,                            //std::usize::MAX in the case of node containing no ids
-
-    label: Sigma,                                   //Sigma::default() for root node                (should never be used at root node)
-    first_occour: Vec<[*mut Node; CARDINALITY]>,    //e.g. first_occour[5][usize::from(Sigma::A)]   (find first fresh occourence of A, 6 levels beneath this node)
-    last_occour: Vec<[*mut Node; CARDINALITY]>,
-    level: Level,                                   //0 for root node
-    next: *mut Node,
-    position: Position,                             //0 for root and first-inserted node at each level
-    parent: *mut Node,
+    id_index_range: Option<(usize, usize)>,
+    label: Sigma,
+    first_occour: Vec<[PosPtr; CARDINALITY]>,   //e.g. first_occour[5][usize::from(Sigma::A)]   (find first fresh occourence of A, 6 levels beneath this node)
+    last_occour: Vec<[PosPtr; CARDINALITY]>,    //first element of first_occour and last_occour both give direct children of the node
+    level: Lvl,                                 //0 for root node
+    next: PosPtr,
+    position: Pos,                              //0 for root and first-inserted node at each level
+    parent: PosPtr,
 }
 impl Node {
     fn new(
-        start_id_index: usize, 
-        end_id_index: usize,
         label: Sigma,
-        level: usize,
-        position: usize,
-        parent: *mut Node,
-        max_level: usize,
+        level: Lvl,
+        position: Pos,
+        parent: PosPtr,
         ) -> Self {
         Node {
-            children: [EMPTY_NODE_LINK; CARDINALITY],
-            first_occour: vec![[ptr::null_mut(); CARDINALITY]; max_level - level],
-            last_occour: vec![[ptr::null_mut(); CARDINALITY]; max_level - level],
-            next: ptr::null_mut(),
-            start_id_index,
-            end_id_index,
+            first_occour: vec![[POS_NULL; CARDINALITY]; ATTR_MAX-level.0],
+            last_occour: vec![[POS_NULL; CARDINALITY]; ATTR_MAX-level.0],
+            next: POS_NULL,
+            id_index_range: None,
             label,
             level,
             position,
@@ -171,6 +163,8 @@ impl Node {
         specifically:
             next
                 use variable with pointer to immediate parent's last_occour for your symbol to get that node and set its "next" pointer to yourself
+                (nope this is wrong, immediate parent won't contain first_occour or last_occour for your symbol which is why your node is empty)
+                (we actually need to go up the stack to the first instance of the same symbol as your node and look at its last occour and then add yourself to the next of that instead)
             first_occour & last occour
                 for all ancestors up to and including first ancestor with same label as the node you added,
                 check first_occour for your symbol at relative level +1 for each ancestor above, if it's null, 
@@ -179,41 +173,42 @@ impl Node {
                 upon reaching the first empty node, check parent's start_id_index and end_id_index,
                 for this range, lookup the associated word in the table and use lexicographical ordering to determine current insert word's position
                 use this to determine which indices to update in order to propperly "insert" id into linked-ids
-                if word happens to be *first* or *last* in this sublist, then chance parent's (and all further relevant ancestors) start_id_index or end_id_index to 
+                if word happens to be *first* or *last* in this sublist, then change parent's (and all further relevant ancestors) start_id_index or end_id_index to 
                 reflect this. (ancestors are only relevant when they have the same start_id_index or end_id_index)
                 for all further empty nodes, start_id_index and end_id_index should just be set to index of the insert word's id in the linked_ids
         
         upon reaching a non-existent node, switch to "create nodes mode" where we know we will have to create a node for all remaining characters
         so here we: create node, then do the same (as above)
         */
+
+        //TODO: figure out how we can make this recursive
     }
-    fn search(&self, query: &[Sigma], ids: &Vec<(Id, usize, usize)>) -> HashSet<Id> {
+    fn search(&self, query: &[Sigma], trie_chunk: &[Vec<Node>], ids: &Vec<(Id, Option<usize>)>) -> HashSet<Id> {
         let mut tuples = HashSet::new();
         if query.len() == 0 {
             return self.tuples_in_subtree(ids);
         }
-        for (level, first) in self.first_occour.iter().enumerate() {
-            let first_ptr = first[usize::from(query[0].to_owned())];
-            if !first_ptr.is_null() {
-                let mut node = first_ptr;
-                unsafe {
-                    tuples = tuples.union(&(*node).search(&query[1..], ids)).cloned().collect(); //TODO: figure out if this is inefficient
-                    while node != self.last_occour[level][usize::from(query[0].to_owned())] {
-                        node = (*node).next;
-                        tuples = tuples.union(&(*node).search(&query[1..], ids)).cloned().collect();
-                    }
+        for (relative_level, first_arr) in self.first_occour.iter().enumerate() {
+            let first_ptr = first_arr[usize::from(query[0].to_owned())];
+            if let Some(Pos(first_pos)) = first_ptr {
+                let mut node = &trie_chunk[relative_level][first_pos];
+                tuples = tuples.union(&node.search(&query[1..], &trie_chunk[relative_level+1..], ids)).cloned().collect(); //TODO: figure out if this is inefficient
+                while node.position.0 != self.last_occour[relative_level][usize::from(query[0].to_owned())].unwrap().0 {
+                    node = &trie_chunk[relative_level][node.next.unwrap().0];
+                    tuples = tuples.union(&node.search(&query[1..], &trie_chunk[relative_level+1..], ids)).cloned().collect();
                 }
             }
         }
         tuples
     }
-    fn tuples_in_subtree(&self, ids: &Vec<(Id, usize, usize)>) -> HashSet<Id> {
+    fn tuples_in_subtree(&self, ids: &Vec<(Id, Option<usize>)>) -> HashSet<Id> {
         let mut tuples = HashSet::new();
-        let mut index = self.start_id_index;
+        let (start_idx, end_idx) = self.id_index_range.unwrap();
+        let mut index = start_idx;
         let mut val = ids[index];
         tuples.insert(val.0);
-        while index != self.end_id_index {
-            index = val.2;
+        while index != end_idx {
+            index = val.1.unwrap();
             val = ids[index];
             tuples.insert(val.0);
         }
